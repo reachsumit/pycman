d4923be30615525788a73219e1340764f5f9128d
Sumit :rocket <sam.sumitkumar@gmail.com>
2019-07-04 19:24:28 -0700

    test commit 2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pycman2",
-    version="0.2",
+    version="0.1",
     author="Sumit Kumar (Arora)",
     author_email="sam.sumitkumar@gmail.com",
     description="Pacman game simulator in Python",
